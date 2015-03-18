67feedc4921d0a30fc7f5851120a04a2501911a5
Ping.X <netten2005@163.com>
2015-03-18 12:00:14 +0800

    Delete test.lua
    no use

--- lnet/model_http/test.lua
+++
@@ -1,7 +0,0 @@
-print(http.req.uri, http.req.content)
-
-http.resp.code = 401
-http.resp.desc = "Unauthorized"
-http.resp.headers["Content-Type"] = "text/plain"
-http.resp.content = "this is a test"
-
